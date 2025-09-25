bc3f538a5047a1c6dc1eafa30e33ff21d2815717
ASYMPTODE <sarthakkale1221@gmail.com>
2025-09-25 22:03:14 +0530

    Fix double slash in API URLs by removing trailing slashes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://style-sphere-backend-xi.vercel.app'
+export const backendUrl = (import.meta.env.VITE_BACKEND_URL || 'https://style-sphere-backend-xi.vercel.app').replace(/\/+$/, '')
 export const currency = '$'
 
 const App = () => {
